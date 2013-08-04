b1330f96898ea550231a8f7afee37af9cf71189b
Jonas Konrad <me@yawk.at>
2013-08-04 23:17:07 +0200

    operations to download chapters and stories

---
+++ src/main/java/at/yawk/fimficiton/operation/AbstractDownloadOperation.java
@@ -0,0 +1,51 @@
+package at.yawk.fimficiton.operation;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import lombok.Cleanup;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import at.yawk.fimficiton.FimFiction;
+
+/**
+ * Abstract base class for downloading content.
+ * 
+ * @author Yawkat
+ */
+@Value
+@NonFinal
+@EqualsAndHashCode(callSuper = false)
+public abstract class AbstractDownloadOperation extends AbstractOperation {
+    @NonNull OutputStream output;
+    @NonNull DownloadType downloadType;
+    
+    @Override
+    public void execute(final FimFiction session) throws IOException {
+        @Cleanup final InputStream input = this.getDownloadUrl().openStream();
+        final byte[] buffer = new byte[1024];
+        int length;
+        while ((length = input.read(buffer)) > 0) {
+            this.getOutput().write(buffer, 0, length);
+        }
+    }
+    
+    /**
+     * Get the {@link URL} to be downloaded.
+     */
+    protected abstract URL getDownloadUrl() throws MalformedURLException;
+    
+    @Override
+    public void reset() {}
+    
+    public static enum DownloadType {
+        EPUB,
+        TEXT,
+        HTML;
+    }
+}
---
+++ src/main/java/at/yawk/fimficiton/operation/ChapterDownloadOperation.java
@@ -0,0 +1,41 @@
+package at.yawk.fimficiton.operation;
+
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import at.yawk.fimficiton.Chapter;
+
+/**
+ * Operation to download a chapter.
+ * 
+ * @author Yawkat
+ */
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class ChapterDownloadOperation extends AbstractDownloadOperation {
+    Chapter chapter;
+    
+    public ChapterDownloadOperation(final OutputStream output, final DownloadType downloadType, final Chapter chapter) {
+        super(output, downloadType);
+        this.chapter = chapter;
+        
+        if (downloadType == DownloadType.EPUB) {
+            throw new IllegalArgumentException("Cannot download chapter as EPUB.");
+        }
+    }
+    
+    @Override
+    protected URL getDownloadUrl() throws MalformedURLException {
+        switch (this.getDownloadType()) {
+        case HTML:
+            return new URL("http://fimfiction.net/download_chapter.php?html&chapter=" + this.getChapter().getId());
+        case TEXT:
+            return new URL("http://fimfiction.net/download_chapter.php?chapter=" + this.getChapter().getId());
+        default:
+            throw new IllegalArgumentException("Unknown download type: " + this.getDownloadType().toString());
+        }
+    }
+}
