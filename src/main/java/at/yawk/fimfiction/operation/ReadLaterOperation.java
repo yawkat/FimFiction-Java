5bd409c21f63e41412fbb311150c36c2016a4e88
Jonas Konrad <me@yawk.at>
2013-11-23 13:32:13 +0100

    Fix ReadLaterOperation

--- src/main/java/at/yawk/fimfiction/operation/ReadLaterOperation.java
+++ src/main/java/at/yawk/fimfiction/operation/ReadLaterOperation.java
@@ -10,7 +10,6 @@ import java.net.URL;
 import java.net.URLConnection;
 
 import lombok.AccessLevel;
-import lombok.Cleanup;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,22 +42,29 @@ public class ReadLaterOperation extends AbstractRequest<Story> {
     protected Story request(final FimFiction session) throws Exception {
         final ReadLaterParser parser = new ReadLaterParser();
         
-        final URL rateUrl = new URL(Util.BASE_URL + "/rate.php");
-        final URLConnection connection = rateUrl.openConnection();
+        final URL url = new URL(Util.BASE_URL + "/ajax/add_read_it_later.php");
+        final URLConnection connection = url.openConnection();
         Util.preparePost(connection);
         connection.setRequestProperty("Cookie", Util.getCookies(session));
         connection.connect();
-        @Cleanup final Writer post = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-        post.write("story=");
-        post.write(Integer.toString(this.getStory().getId()));
-        post.write("&selected=");
-        post.write(this.isReadLater() ? "1" : "0");
-        @Cleanup final Reader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-        final XMLReader xmlReader = new Parser();
-        xmlReader.setContentHandler(parser);
-        xmlReader.parse(new InputSource(reader));
-        
-        return this.story.withReadLater(parser.isSelected());
+        final Writer post = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        try {
+            post.write("story=");
+            post.write(Integer.toString(this.getStory().getId()));
+            post.write("&selected=");
+            post.write(this.isReadLater() ? "1" : "0");
+        } finally {
+            post.close();
+        }
+        final Reader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        try {
+            final XMLReader xmlReader = new Parser();
+            xmlReader.setContentHandler(parser);
+            xmlReader.parse(new InputSource(reader));
+            return this.story.withReadLater(parser.isSelected());
+        } finally {
+            reader.close();
+        }
     }
 }
 
@@ -71,14 +77,8 @@ class ReadLaterParser extends DefaultHandler {
     
     @Override
     public void startElement(final String uri, final String localName, final String qName, final Attributes atts) {
-        if (qName.equals("likes")) {
+        if (qName.equals("selected")) {
             this.stage = 1;
-        } else if (qName.equals("dislikes")) {
-            this.stage = 2;
-        } else if (qName.equals("liked")) {
-            this.stage = 3;
-        } else if (qName.equals("disliked")) {
-            this.stage = 4;
         }
     }
     
