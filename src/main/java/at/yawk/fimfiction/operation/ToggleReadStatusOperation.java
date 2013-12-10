4642b37de667cb3f77e92506eaff77bec72d9efb
Jonas Konrad <me@yawk.at>
2013-12-10 16:24:17 +0100

    Fix unread status toggle

--- src/main/java/at/yawk/fimfiction/operation/ToggleReadStatusOperation.java
+++ src/main/java/at/yawk/fimfiction/operation/ToggleReadStatusOperation.java
@@ -1,17 +1,23 @@
 package at.yawk.fimfiction.operation;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.Value;
+
+import org.ccil.cowan.tagsoup.Parser;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
 import at.yawk.fimfiction.Chapter;
 import at.yawk.fimfiction.FimFiction;
 
@@ -41,14 +47,24 @@ public class ToggleReadStatusOperation extends AbstractRequest<Chapter> {
             post.close();
         }
         
-        final Reader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+        final InputStream stream = connection.getInputStream();
         try {
-            final char[] buffer = new char[100];
-            final int length = reader.read(buffer);
-            final String resultingImageSource = new String(buffer, 0, length);
-            return this.chapter.withUnread(!resultingImageSource.endsWith("tick.png"));
+            final XMLReader r = new Parser();
+            final AtomicBoolean unread = new AtomicBoolean(this.chapter.isUnread());
+            r.setContentHandler(new DefaultHandler() {
+                @Override
+                public void characters(final char[] ch, final int start, final int length) throws SAXException {
+                    if (length == 1) {
+                        if (ch[start] == '1' || ch[start] == '0') {
+                            unread.set(ch[start] == '0');
+                        }
+                    }
+                }
+            });
+            r.parse(new InputSource(stream));
+            return this.chapter.withUnread(unread.get());
         } finally {
-            reader.close();
+            stream.close();
         }
     }
 }
